49e981aebd83908221b42ba0a6c3a2b73eb7f2db
longbang <454595025@qq.com>
2019-01-25 05:17:48 -0800

    no message

--- src/resources/views/phaser/layouts/default.blade.php
+++ src/resources/views/phaser/layouts/default.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>@yield('title')</title>
-    <script src="{{Lottery::getVccAssets('js/jquery-1.12.4.min.js')}}"></script>
-    <link rel="stylesheet" href="{{Lottery::getVccAssets('css/init.min.css')}}">
-    <link rel="stylesheet" href="{{Lottery::getVccAssets('css/vcc_a.min.css')}}">
-    <link rel="stylesheet" href="{{Lottery::getVccAssets('font/iconfont.min.css')}}">
-    <script src="{{Lottery::getVccAssets('font/iconfont.min.js')}}"></script>
-    <script src="{{Lottery::getVccAssets('js/vcc_a.min.js')}}"></script>
-    <script src="{{Lottery::getVccAssets('js/vcc_b.min.js')}}"></script>
+    <script src="{{Vcc::getAssets('js/jquery-1.12.4.min.js')}}"></script>
+    <link rel="stylesheet" href="{{Vcc::getAssets('css/init.min.css')}}">
+    <link rel="stylesheet" href="{{Vcc::getAssets('css/vcc_a.min.css')}}">
+    <link rel="stylesheet" href="{{Vcc::getAssets('font/iconfont.min.css')}}">
+    <script src="{{Vcc::getAssets('font/iconfont.min.js')}}"></script>
+    <script src="{{Vcc::getAssets('js/vcc_a.min.js')}}"></script>
+    <script src="{{Vcc::getAssets('js/vcc_b.min.js')}}"></script>
     <script src="{{Lottery::getAssets('js/phaser2.6.2.min.js')}}"></script>
 </head>
 <body>
